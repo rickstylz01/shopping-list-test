0dcb47dfecf363a2cb9de5f280fbea3e509f2890
Rick Maya <rickmaya@gmail.com>
2021-01-18 15:32:55 -0600

    initial commit, create tests for components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import AddItemForm from './shopping-list/AddItemForm';
+import ShoppingList from './shopping-list/ShoppingList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
+  render() {
+    return (
+      // unnecessary div called fragment
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm />
+          </section>
+          <section>
+            <ShoppingList items={this.state.shoppingItems} />
+          </section>
+        </main>
+      </>
+    )
+  }
 }
+
 
 export default App;
